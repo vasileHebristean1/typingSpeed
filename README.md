4737a8717253107f7e430a4fd207d0a8e7c53973
Hebristean Vasile <79838394+vasileHebristean1@users.noreply.github.com>
2021-09-01 07:28:28 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# typingSpeed
+Installing
+
+Download the Python 3 installer package from the official website and install it, if not installed previously.
+
+Running the application
+
+Download the source code from the repository and run the file just as any other Python script (.py) file.
+
+typingSpeed.py
---
+++ typingSpeed.py
@@ -0,0 +1,48 @@
+from tkinter import *
+from timeit import default_timer as timer
+import random
+
+window = Tk()
+window_geometry = ("1200x800")
+x = 0
+
+def game():
+    global x
+    if x == 0:
+        window_destroy()
+        x = x+1
+
+def check_result():
+    if entry.get() == words[word]:
+        end = timer()
+        print(end-start)
+    else:
+        print("Wrong input")
+
+words = ['programming', 'coding', 'algorithm',
+             'systems', 'python', 'software']
+
+word = random.randint(0, len(words)-1)
+start = timer()
+windows = Tk()
+windows_geometry = ("1200x800")
+x2 = Label(windows, text=words[word], font="times 20")
+x2.place(x=150, y=10)
+x3 = Label(windows, text="Start typing", font="times 20")
+x3.place(x=10, y=50)
+
+entry = Entry(windows)
+entry.place(x=280, y=55)
+b2 = Button(windows, text="Done", command=check_result, width=12, bg="grey")
+b2.place(x=150, y=100)
+b3 = Button(windows, text="Try again", command=game, width=12, bg="grey")
+b3.place(x=250, y=100)
+windows.mainloop()
+
+x1 = Label(window, text="Let's start playing...", font="times 20")
+x1.place(x=10, y=50)
+
+b1 = Button(window, text="Go", command=game, width=12, bg="grey")
+b1.place(x=150, y=100)
+
+window.mainloop()
